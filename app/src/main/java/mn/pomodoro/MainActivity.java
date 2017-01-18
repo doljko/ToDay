bbf3f4116c6394bb2b6bb792a25ef20db87ab958
doljko <doljko_tv@yahoo.com>
2017-01-18 21:38:45 +0800

    Main activity цэснүүдийг ажилладаг болгов

--- app/src/main/java/mn/pomodoro/MainActivity.java
+++ app/src/main/java/mn/pomodoro/MainActivity.java
@@ -1,5 +1,6 @@
 package mn.pomodoro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,14 +81,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_graph) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
+            Intent intent = new Intent(this, GraphicActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_manage) {
-
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
