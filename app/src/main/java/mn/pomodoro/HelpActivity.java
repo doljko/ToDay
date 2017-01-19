e944a2b1aa1dfd364ea8901134b3960c65b2195d
doljko <doljko_tv@yahoo.com>
2017-01-19 21:45:02 +0800

    Float button устгаж Логин активтив засав

--- app/src/main/java/mn/pomodoro/HelpActivity.java
+++ app/src/main/java/mn/pomodoro/HelpActivity.java
@@ -16,14 +16,7 @@ public class HelpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
