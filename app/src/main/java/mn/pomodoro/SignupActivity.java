5970a29c6cb7b2a49a6cfdaf6e5a15c3f65fcc71
doljko <doljko_tv@yahoo.com>
2017-01-23 22:10:37 +0800

    signup function created

--- app/src/main/java/mn/pomodoro/SignupActivity.java
+++ app/src/main/java/mn/pomodoro/SignupActivity.java
@@ -1,6 +1,9 @@
 package mn.pomodoro;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,34 +14,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.w3c.dom.Text;
 
 public class SignupActivity extends AppCompatActivity {
+    EditText usernameText;
+    EditText emailAddress;
+    EditText passOneTxt;
+    EditText passReenter;
+    Button signUpButton;
+    TextView loginButton;
 
+    public SharedPreferences sharedPreferences;
+    Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        EditText usernameText = (EditText)findViewById(R.id.signUpName);
-        EditText emailAddress = (EditText)findViewById(R.id.emailAddress);
-        EditText passOneTxt = (EditText)findViewById(R.id.passOneTxt);
-        EditText passReenter = (EditText)findViewById(R.id.passReenter);
-        Button signUpButton = (Button)findViewById(R.id.signUpButton);
-        TextView loginButton = (TextView)findViewById(R.id.signUpLoginButton);
+        usernameText = (EditText)findViewById(R.id.signUpName);
+        emailAddress = (EditText)findViewById(R.id.emailAddress);
+        passOneTxt = (EditText)findViewById(R.id.passOneTxt);
+        passReenter = (EditText)findViewById(R.id.passReenter);
+        signUpButton = (Button)findViewById(R.id.signUpButton);
+        loginButton = (TextView)findViewById(R.id.signUpLoginButton);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
+               signup();
             }
         });
 
@@ -50,4 +57,69 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void signup(){
+        if (!validate()) {
+//            onSignupFailed();
+            Toast.makeText(getBaseContext(), "Алдаа гарлаа", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final String name = usernameText.getText().toString();
+        String email = emailAddress.getText().toString();
+        final String pass = passOneTxt.getText().toString();
+        String passConfirm = passReenter.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+//                        onSignupSuccess();
+                        sharedPreferences = getSharedPreferences("User", 0);
+                        editor = sharedPreferences.edit();
+                        editor.putString("username", name);
+                        editor.putString("password", pass);
+                        editor.commit();
+
+                        Toast.makeText(getBaseContext(), "Амжилттай бүртгэгдлээ", Toast.LENGTH_LONG).show();
+                    }
+                }, 3000);
+    }
+
+//    public void onSignupSuccess() {
+//        signUpButton.setEnabled(true);
+//        setResult(RESULT_OK, null);
+//        Toast.makeText(getBaseContext(), "Амжилттай бүртгэгдлээ", Toast.LENGTH_LONG).show();
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        finish();
+//                    }
+//                }, 3000);
+//
+//    }
+
+    public boolean validate (){
+        boolean valid = true;
+
+        String name = usernameText.getText().toString();
+        String email = emailAddress.getText().toString();
+        String pass = passOneTxt.getText().toString();
+        String passConfirm = passReenter.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3){
+            usernameText.setError("Нэр оруулна уу");
+            valid = false;
+        }
+        else {
+            usernameText.setError(null);
+        }
+        return valid;
+
+    }
+
+//    public void onSignupFailed() {
+//        Toast.makeText(getBaseContext(), "Бүртгүүлэхэд алдаа гарлаа", Toast.LENGTH_LONG).show();
+//
+//    }
 }
