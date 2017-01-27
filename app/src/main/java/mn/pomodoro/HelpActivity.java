4902b1f7f40a468e2405192bf70a9fc81e62e2b4
doljko <doljko_tv@yahoo.com>
2017-01-27 10:28:11 +0800

    Help.n zalgaxiig hiiv

--- app/src/main/java/mn/pomodoro/HelpActivity.java
+++ app/src/main/java/mn/pomodoro/HelpActivity.java
@@ -1,11 +1,17 @@
 package mn.pomodoro;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -16,7 +22,28 @@ public class HelpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button call = (Button) findViewById(R.id.help_call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calling();
+            }
+        });
+    }
 
+    public void  calling(){
+        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:89860933"));
+        if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 
 }
