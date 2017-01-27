764127275a7b458dd9594fac6f75ff2e4f85a597
doljko <doljko_tv@yahoo.com>
2017-01-27 09:51:17 +0800

    help layout components added

--- app/src/main/java/mn/pomodoro/MainActivity.java
+++ app/src/main/java/mn/pomodoro/MainActivity.java
@@ -133,6 +133,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_help) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
