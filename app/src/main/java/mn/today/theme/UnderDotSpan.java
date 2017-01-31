22e9929f38e0cf6d7c57f1dfbedb4b19e2310b81
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-01-31 12:53:55 +0800

    Text span that draws a dot under text

---
+++ app/src/main/java/mn/today/theme/UnderDotSpan.java
@@ -0,0 +1,54 @@
+package mn.today.theme;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+import android.text.style.ReplacementSpan;
+
+import mn.today.R;
+
+/**
+ * Created by Tortuvshin Byambaa on 1/31/2017.
+ */
+public class UnderDotSpan extends ReplacementSpan {
+
+    private final float mSize;
+    private final int mDotColor;
+    private final int mTextColor;
+
+    public UnderDotSpan(Context context) {
+        super();
+        TypedArray ta = context.getTheme().obtainStyledAttributes(new int[]{
+                R.attr.colorAccent,
+                android.R.attr.textColorPrimary
+        });
+        mDotColor = ta.getColor(0, ContextCompat.getColor(context, R.color.greenA700));
+        //noinspection ResourceType
+        mTextColor = ta.getColor(1, 0);
+        ta.recycle();
+        mSize = context.getResources().getDimension(R.dimen.dot_size);
+    }
+
+    @Override
+    public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
+        return Math.round(paint.measureText(text, start, end));
+    }
+
+    @Override
+    public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+        if (TextUtils.isEmpty(text)) {
+            return;
+        }
+        float textSize = paint.measureText(text, start, end);
+        paint.setColor(mDotColor);
+        canvas.drawCircle(x + textSize / 2, // text center X
+                bottom + mSize, // dot center Y
+                mSize / 2, // radius
+                paint);
+        paint.setColor(mTextColor);
+        canvas.drawText(text, start, end, x, y, paint);
+    }
+}
