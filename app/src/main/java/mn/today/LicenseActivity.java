119e84acba39cda2a9459fb5e5e22985205adad2
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-02-13 13:06:59 +0800

    licence activity created

---
+++ app/src/main/java/mn/today/LicenseActivity.java
@@ -0,0 +1,33 @@
+package mn.today;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by Tortuvshin Byambaa on 2/15/2017.
+ */
+
+public class LicenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_license);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.license_activity_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
