2db6eaef72b7dd27e5b99138063b2bd89c604154
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-02-20 23:19:33 +0800

    preference manager class

---
+++ app/src/main/java/mn/today/PrefManager.java
@@ -0,0 +1,38 @@
+package mn.today;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Tortuvshin Byambaa on 2/20/2017.
+ */
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "today";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
